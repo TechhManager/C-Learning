de087f53f4cfe9e65968fcc99402c5d6455c0c68
TechhManager <tech@inside.com>
2025-11-20 15:05:26 +0530

    learning till 1.5

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+
+int main (){
+
+    std::cout << "Enter a number";
+    int x {};
+    int y {};
+    int z {};
+
+    std:: cin >> x >>y >>z;
+    std:: cout << "You entered " << x << " , " << y << " and " << z << ". \n";
+    return 0;
+}
+
